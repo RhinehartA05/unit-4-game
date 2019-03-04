e314238a31be458a73aa8a8ed2d45fd5b19f1b4c
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-03-04 00:04:29 -0500

    updated html,javascript and css files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,93 @@
+$(document).ready(function(){
 
-//who are the characters
-var characterOne = {
-    charOneName: "Obi-Wan", //Character name
-    charOneHealth: 120, //Character health
-    charOneValue: 25, //Character attack value
-  };
-var characterTwo = {
-    charTwoName: "Luke",
-    charTwoHealth: 100,
-    charTwoValue: 25,
-  };
-var characterThree = {
-    charThreeName: "Sidious",
-    charThreeHealth: 150,
-    charThreeValue: 25,
-  };
-var characterFour = {
-    charFourName: "Maul",
-    charFourHealth: 180,
-    charFourValue: 25,
-  };
+var obiWanImg = $('#obiWan');
+var lukeImg = $('#luke');
+var sidiousImg = $('#sidious');
+var maulImg = $('#maul');
+
+var obiWanImgSel = $('#obiWanSelect');
+var lukeImgSel = $('#lukeSelect');
+var sidiousImgSel = $('#sidiousSelect');
+var maulImgSel = $('#maulSelect');
+
+
+//who are the fighters
+var fighters = {
+    obiWan: {
+        health: 120,
+        value: 25,
+    },
+    luke: {
+        health: 100,
+        value: 25,
+    },
+    sidious: {
+        health: 150,
+        value: 25,
+    },
+    maulImg: {
+        health: 180,
+        value: 25,
+    }
+};
+
 
 //what positions are establish?
-var slot1 = "selection-screen" //All Characters are here
-var slot2 = "attacker" //The player selected character
-var slot3 = "defender" //The players selected opponent
+var slot1 = "" //All Characters are here
+var slot2 = "" //Selected Character
+var slot3 = "" //Selected Opponent
+var userAttack = 0;
+var wins = 0;
+var losses = 0;
 
 
+//attack function
+var attackBtn = $('#attackBtn');
 
+// $(".fighter").click(function(){
+//     $(this).hide();
+//     $(".photo2").show();
+//     });
 
+//selected character is removed from selection screen and selected fighter image appears.
+    $("#obiWan").click(function(){
+        $(this).hide();
+        $("#obiWanSelect").show();
+        $("#playerSelected").append(obiWanImgSel);
+        });
 
-
-
-
-$(document).ready(function(){
+    $("#luke").click(function(){
+        $(this).hide();
+        $("#lukeSelect").show();
+        $( "#playerSelected" ).append(lukeImgSel);
+        });
     
-    $(".photo1").click(function(){
-    $(this).hide();
-    }); 
+        $("#sidious").click(function(){
+        $(this).hide();
+        $("#sidiousSelect").show();
+        $( "#playerSelected" ).append(sidiousImgSel);
+        });
+    
+    
+        $("#maul").click(function(){
+        $(this).hide();
+        $("#maulSelect").show();
+        $( "#playerSelected" ).append(maulImgSel);
+        });
+    
+   
 
 
+
+
+    
+    // $(".photo1").click(function(){
+    // $(this).hide();
+    // }); 
+
+    // $("#attackBtn").click(function(){
+    //     $(this).hide();
+    //     }); 
 
 
 
