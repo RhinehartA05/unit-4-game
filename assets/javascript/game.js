832b33edbfa0a913fd7ea8f9de56639009e9c881
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-03-03 19:18:14 -0500

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+
+//who are the characters
+var characterOne = {
+    charOneName: "Obi-Wan", //Character name
+    charOneHealth: 120, //Character health
+    charOneValue: 25, //Character attack value
+  };
+var characterTwo = {
+    charTwoName: "Luke",
+    charTwoHealth: 100,
+    charTwoValue: 25,
+  };
+var characterThree = {
+    charThreeName: "Sidious",
+    charThreeHealth: 150,
+    charThreeValue: 25,
+  };
+var characterFour = {
+    charFourName: "Maul",
+    charFourHealth: 180,
+    charFourValue: 25,
+  };
+
+//what positions are establish?
+var slot1 = "selection-screen" //All Characters are here
+var slot2 = "attacker" //The player selected character
+var slot3 = "defender" //The players selected opponent
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+  $("#para").click(function(){
+    $(this).hide();
+  });
